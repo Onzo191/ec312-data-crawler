b06e5f2f8947d714292b965c73775b9d538899d5
Faryn-03 <anhphuocdang@gmail.com>
2024-04-18 16:49:02 +0700

    Create script.js
    test :vvv

---
+++ public/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $("#downloadButton").hide();
+
+  $(".card-form").submit(function (e) {
+      e.preventDefault();
+
+      $("#processing").html("<h3>Processing...</h3>");
+
+      $.ajax({
+          url: "http://localhost:4000/api/v1/crawler",
+          method: "POST",
+          contentType: "application/json",
+          data: JSON.stringify({
+              keyword: $("#keyword").val(),
+              quantity: $("#quantity").val(),
+              website: $("#website").val(),
+          }),
+          success: function (response) {   
+              $("#processing").html("<h3>Successful!</h3>");
+              $("#downloadButton").show();
+              $("#downloadButton").removeClass("d-none");
+              $("#downloadLink").attr("href", response.downloadLink);
+          },
+          error: function (error) {
+              $("#processing").html("<h3>An unexpected error occurred. Please try again later.</h3>");
+          },
+      });
+  });
+});
